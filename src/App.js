bf1965b85e22dc15b25b39c4e04a71f37c7444d4
Jagerziel <fromnytosg@gmail.com>
2023-06-19 12:24:17 -0400

    added useState to track checkbox

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState , useEffect } from "react";
 
 function App() {
 
+  // Hold Checkbox Instances
+  const [ instanceCheckbox , setInstanceCheckbox ] = useState({
+    instance01: false,
+    instance02: false,
+    instance03: false,
+    instance04: false,
+    instance05: false
+  })
+  // console.log(instanceCheckbox)
 
   return (
     <div className="App">
